4ae4191577a75b02ced9397343ef6166a98864e3
Taku Yamada <tmn.sby001@gmail.com>
2017-10-14 14:44:26 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by 山田卓 on 2017/10/14.
+//  Copyright © 2017 TakuYamada. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
