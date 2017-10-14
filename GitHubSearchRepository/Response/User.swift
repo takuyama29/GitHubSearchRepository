b625b577fabbd2ee880e396c4572941332b9a20c
Taku Yamada <tmn.sby001@gmail.com>
2017-10-14 17:01:37 +0900

    Definition initialize of user type

--- GitHubSearchRepository/Response/User.swift
+++ GitHubSearchRepository/Response/User.swift
@@ -8,7 +8,25 @@
 
 import Foundation
 
-struct User {
+struct User { // JSONを受け取ってユーザー情報をインスタンス化するための型
     let id: Int
     let login: String
+    
+    // JSONのフォーマットに合わせてイニシャライズする
+    init(json: Any) throws { // JSONが意図しないフォーマットになった場合に例外が発生しうるのでthrow文を使う
+        guard let dictionaly = json as? [String :  Any] else {
+            throw JSONDecodeError.invalidFormat(json: json)
+        }
+        guard let id = dictionaly["id"] as? Int else {
+            throw JSONDecodeError.missingValue(
+                key: "id", actualValue: dictionaly["id"])
+        }
+        
+        guard let login = dictionaly["login"] as? String else {
+            throw JSONDecodeError.missingValue(key: "login", actualValue: dictionaly["login"])
+        }
+        
+        self.id = id
+        self.login = login
+    }
 }
